1d3ffca2c28669c9a29f0760305d326dff9d9c92
Ricardo <137570173+Hatusil@users.noreply.github.com>
2024-10-24 13:34:47 +0000

    agregando el grid

--- src/TeamCards.jsx
+++ src/TeamCards.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './TeamCards.css';
 
 const TeamCards = () => {
   return (
@@ -10,6 +11,26 @@ const TeamCards = () => {
         sí mismos, sus habilidades y enlaces a sus perfiles profesionales.
       </p>
 
+      <div className='teams-cards-container'>
+        <div className='random-card'>
+          <h2 className="card-name">Card 1</h2>
+        </div>
+        <div className='random-card'>
+          <h2 className="card-name">Card 2</h2>
+        </div>
+        <div className='random-card'>
+          <h2 className="card-name">Card 3</h2>
+        </div>
+        <div className='random-card'>
+          <h2 className="card-name">Card 4</h2>
+        </div>
+        <div className='random-card'>
+          <h2 className="card-name">Card 5</h2>
+        </div>
+        <div className='random-card'>
+          <h2 className="card-name">Card 6</h2>
+        </div>
+      </div>
       {/* Agregar más tarjetas cuando los miembros del equipo las proporcionen */}
     </div>
   );
