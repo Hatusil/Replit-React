6a5fe21f3804a5468d8db6bcfda578ac7c6cc203
Ricardo <137570173+Hatusil@users.noreply.github.com>
2024-10-25 01:29:53 +0000

    agregando el logo

--- src/TeamCards.jsx
+++ src/TeamCards.jsx
@@ -21,8 +21,8 @@ const TeamCards = () => {
     <div className="team-cards">
       <img 
         className="team-logo" 
-        src="https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/e5/46/51/e5465126-9028-eefa-ab2c-1f68ead463ee/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/512x512bb.jpg" 
-        alt="Logo de HiFiveDevelopers" 
+        src="./public/img/logo.png"
+        alt="Team Logo"
       />
       <h1 className="team-title">Bienvenidos a HiFiveDevelopers</h1>
       <p className="team-intro">
