a4c1a3423521a418cf894d16bd1c8f15fa1c60cb
Ricardo <137570173+Hatusil@users.noreply.github.com>
2024-10-25 01:10:52 +0000

    agregando logo de equipo

--- src/TeamCards.jsx
+++ src/TeamCards.jsx
@@ -19,9 +19,16 @@ const TeamCards = () => {
 
   return (
     <div className="team-cards">
+      <img 
+        className="team-logo" 
+        src="https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/e5/46/51/e5465126-9028-eefa-ab2c-1f68ead463ee/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/512x512bb.jpg" 
+        alt="Logo de HiFiveDevelopers" 
+      />
       <h1 className="team-title">Bienvenidos a HiFiveDevelopers</h1>
       <p className="team-intro">
-        Conoce a nuestro talentoso equipo de desarrolladores.
+        Conoce a nuestro talentoso equipo de desarrolladores. Aquí podrás ver 
+        las tarjetas personales de cada integrante, donde comparten un poco sobre 
+        sí mismos, sus habilidades y enlaces a sus perfiles profesionales.
       </p>
 
       <div className='teams-cards-container'>
